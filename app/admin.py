4f1cdc432020d3135c924b051e741f7b74daf316
Dilmurod <@eshmamatovdilmurod7@gmail.com>
2025-04-10 19:06:27 +0500

    model added

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    list_display = 'id', 'number', 'is_active'
+    list_display_links = 'id', 'number', 'is_active'
+
+
+@admin.register(New)
+class AdminNew(admin.ModelAdmin):
+    list_display = 'title', 'description'
+    list_display_links = 'title', 'description'
+
+
+@admin.register(Branch)
+class AdminBranch(admin.ModelAdmin):
+    list_display = 'name', 'address', 'start_work', 'end_work'
+    list_display_links = 'name', 'address', 'start_work', 'end_work'
+
+
+@admin.register(Certificate)
+class AdminCertificate(admin.ModelAdmin):
+    list_display = 'description', 'order'
+    list_display_links = 'description', 'order'
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = 'name',
+    list_display_links = 'name',
+
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = 'title', 'price', 'category'
+    list_display_links = 'title', 'price', 'category'
--- app/models.py
+++ app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    number = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+
+
+class New(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Branch(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField()
+    lat = models.CharField(max_length=255)
+    lng = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    start_work = models.DateTimeField()
+    end_work = models.DateTimeField()
+    created_at = models.DateTimeField()
+
+
+class Certificate(models.Model):
+    description = models.TextField()
+    image = models.ImageField()
+    order = models.PositiveIntegerField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(255)
+    image = models.ImageField(upload_to='product_img')
+    price = models.DecimalField(max_digits=10, decimal_places=10)
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="old_orders", on_delete=models.CASCADE,
+    )
+
+
